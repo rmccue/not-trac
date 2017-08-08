24ecbfd9d21b6b00e0c4e5593a56ceaf8bd29057
Ryan McCue <me@ryanmccue.info>
2017-08-08 15:13:09 +1000

    Add full date in title of Time

--- src/components/Time.js
+++ src/components/Time.js
@@ -3,5 +3,9 @@ import React from 'react';
 
 export default ({ timestamp }) => {
 	const date = moment.unix( timestamp );
-	return <time dateTime={ date.toISOString() }>{ date.fromNow() }</time>
+	return <time
+		children={ date.fromNow() }
+		dateTime={ date.toISOString() }
+		title={ date.format( 'LLLL' ) }
+	/>;
 };
